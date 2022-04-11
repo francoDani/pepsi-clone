8faf5ec9de8e440fcddde1255079cc34219cc864
Franco <franco.danielsen@gmail.com>
2022-04-11 18:31:14 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function changeImg (imagen){
+    document.querySelector('.pepsi').src= imagen;
+}
+
+function changeBgColor (color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
